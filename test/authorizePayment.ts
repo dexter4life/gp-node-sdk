063e61d8965ff023d42844a3d20fd2994145de84
Peter Chukwuemeka Nwanosike <dexter4life@gmail.com>
2023-02-01 00:48:01 +0100

    Update authorizePayment.ts

--- test/authorizePayment.ts
+++ test/authorizePayment.ts
@@ -3,10 +3,10 @@ import { CreditCardData, ServicesConfig, ServicesContainer } from "../src";
 // Card Data
 async function authorizePayment() {
   const config = new ServicesConfig();
-  config.appKey = "QVefNGo6bkMcjveA";
+  config.appKey = "";
   config.serviceUrl = "https://apis.sandbox.globalpay.com/ucp";
   config.version = "2021-03-22";
-  config.applicationId = "i9R0byBBor6RqTQNj3g4MuVBwH5rd7yR";
+  config.applicationId = "";
 
   ServicesContainer.configure(config);
 
