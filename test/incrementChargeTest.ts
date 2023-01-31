c1a640cbe871d1b1f2f5c8d1625d539f9fc51105
Peter Chukwuemeka Nwanosike <dexter4life@gmail.com>
2023-02-01 00:47:33 +0100

    Update incrementChargeTest.ts

--- test/incrementChargeTest.ts
+++ test/incrementChargeTest.ts
@@ -2,10 +2,10 @@ import { ServicesConfig, ServicesContainer, Transaction } from "../src";
 
 async function incrementChargeTest() {
   const config = new ServicesConfig();
-  config.appKey = "QVefNGo6bkMcjveA";
+  config.appKey = "";
   config.serviceUrl = "https://apis.sandbox.globalpay.com/ucp";
   config.version = "2021-03-22";
-  config.applicationId = "i9R0byBBor6RqTQNj3g4MuVBwH5rd7yR";
+  config.applicationId = "";
 
   ServicesContainer.configure(config);
 
